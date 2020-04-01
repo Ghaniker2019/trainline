cd1227ea8c71b4b86ac99cb6bb247216edb52d77
kerkoub <kerkoub.abdelghani@gmail.com>
2020-04-01 07:06:23 +0200

    Updates

---
+++ precache-manifest.51375b0fe0c9862b0bf84ff222ed737a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19e71ca852f59762ae343235c8e4de7a",
+    "url": "/trainline/index.html"
+  },
+  {
+    "revision": "28ff896197172c432545",
+    "url": "/trainline/static/css/2.d55b1fd7.chunk.css"
+  },
+  {
+    "revision": "77c17cfec80317f63eb8",
+    "url": "/trainline/static/css/main.839f9561.chunk.css"
+  },
+  {
+    "revision": "28ff896197172c432545",
+    "url": "/trainline/static/js/2.768df3d5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/trainline/static/js/2.768df3d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77c17cfec80317f63eb8",
+    "url": "/trainline/static/js/main.e172ed19.chunk.js"
+  },
+  {
+    "revision": "5b49cba7f00e75e429ba",
+    "url": "/trainline/static/js/runtime-main.10a8efd5.js"
+  },
+  {
+    "revision": "982386e2d4b16d8a061d83647e35c39c",
+    "url": "/trainline/static/media/SourceSansPro-Regular.982386e2.ttf"
+  },
+  {
+    "revision": "b742ccc5606c3d124fabbcb146ae868b",
+    "url": "/trainline/static/media/train.b742ccc5.jpg"
+  }
+]);
